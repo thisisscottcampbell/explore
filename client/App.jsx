595eb6ed98cd88819aa975b694dceadab9593189
christygomez <christygomez@gmail.com>
2021-01-06 22:03:43 -0500

    pause for main pull

--- client/App.jsx
+++ client/App.jsx
@@ -4,6 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import LoginPage from './containers/LoginPage';
 import SignupPage from './containers/SignupPage';
 import HomePage from './containers/HomePage';
+import Profile from './constainers/time/Profile';
 import TimeHomePage from './containers/time/TimeHomePage';
 import PrivateRoute from './privateRoute';
 import NotFound from './containers/404';
@@ -11,108 +12,114 @@ import NotFound from './containers/404';
 import TripPage from './containers/time/TripPage';
 import ActivityList from './containers/time/Activities/ActivityList';
 
-class App extends Component { 
+class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      trips : [], 
-      message : '',
-    }
+      trips: [],
+      message: '',
+    };
   }
 
   handleNewTrip = (trips) => {
-    this.setState({trips : trips})
-  }
+    this.setState({ trips: trips });
+  };
 
   handleAddedActivity = (trips) => {
-    this.setState({trips : trips})
-  }
+    this.setState({ trips: trips });
+  };
 
   handleStateUpdate = (emptyTrip) => {
-    this.setState({trips: emptyTrip})
-  }
+    this.setState({ trips: emptyTrip });
+  };
 
   handleDelete = (trips, message) => {
-    this.setState({trips: trips, message: message}, () => console.log(this.state));
-  }
-t
+    this.setState({ trips: trips, message: message }, () =>
+      console.log(this.state)
+    );
+  };
+  t;
   handleFetchState = () => {
     fetch('/api/trips/')
-    .then(response => response.json())
-    .then((result) => {
-      const {trips} = result;
-      const emptyTrip =[];
-      trips.forEach(trip => {
-        const newTrip = {}
-        newTrip.location = trip.destination
-        newTrip.tripName = trip.title
-        newTrip.place_id = trip.place_id
-        newTrip.tripStartFrontEnd = trip.start_date
-        newTrip.tripEndFrontEnd = trip.end_date
-        newTrip.locationphotos = trip.locationphotos
-        newTrip.datesKnown = trip.dates_known
-        newTrip.id = trip.id
-        emptyTrip.push(newTrip);
+      .then((response) => response.json())
+      .then((result) => {
+        const { trips } = result;
+        const emptyTrip = [];
+        trips.forEach((trip) => {
+          const newTrip = {};
+          newTrip.location = trip.destination;
+          newTrip.tripName = trip.title;
+          newTrip.place_id = trip.place_id;
+          newTrip.tripStartFrontEnd = trip.start_date;
+          newTrip.tripEndFrontEnd = trip.end_date;
+          newTrip.locationphotos = trip.locationphotos;
+          newTrip.datesKnown = trip.dates_known;
+          newTrip.id = trip.id;
+          emptyTrip.push(newTrip);
+        });
+        this.handleStateUpdate(emptyTrip);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
       });
-      this.handleStateUpdate(emptyTrip);
-  })
-  .catch((error) => {
-    console.error('Error:', error);
-  }); 
-}
+  };
 
   handleFetchYelp = (location, tripId) => {
     // console.log(this.state);
     fetch('/api/yelp/' + location, {
-      method: "GET",
+      method: 'GET',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
       },
     })
-    .then(response => response.json())
-    .then((result) => {
-    
-    // console.log('result', result);
-    let trips = [...this.state.trips]
-    let trip = trips.filter( el => el.id === tripId)
-    trip = trip[0]
-    let index = this.state.trips.indexOf(trip);
-    let newActivites = result.result;
-    trip.activities = newActivites;
-    trips.splice(index, 1, trip);
-    this.setState({trips});
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    }); 
-  }
-
-
+      .then((response) => response.json())
+      .then((result) => {
+        // console.log('result', result);
+        let trips = [...this.state.trips];
+        let trip = trips.filter((el) => el.id === tripId);
+        trip = trip[0];
+        let index = this.state.trips.indexOf(trip);
+        let newActivites = result.result;
+        trip.activities = newActivites;
+        trips.splice(index, 1, trip);
+        this.setState({ trips });
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  };
 
   render() {
-  return(
-  <div id="app" className="main-container">
-    <Switch>
-      <Route path="/" exact component={HomePage} />
-      <Route path="/login" exact component={LoginPage} />
-      <Route path="/signup" exact component={SignupPage} />
-      {/* <Route path="/resetpassword" exact component={ResetPasswordPage} /> */}
-      <PrivateRoute path="/time/home"  component={TimeHomePage} trips = {this.state.trips}
-        handleNewTrip = {this.handleNewTrip}
-        handleStateUpdate = {this.handleStateUpdate}
-        handleDelete = {this.handleDelete}
-        handleFetchState = {this.handleFetchState} />
-      {/* <PrivateRoute path="/time/trip" exact component={TripPage} /> */}
-      <PrivateRoute path="/time/trip/:tripId" component={TripPage}  
-        handleFetchYelp = {this.handleFetchYelp}
-        handleAddedActivity = {this.handleAddedActivity}
-      />
-       {/* <PrivateRoute path="/time/activitylist" exact component={ActivityList} /> */}
-      <PrivateRoute path="*" component={NotFound} />
-    </Switch>
-  </div>
-  )
+    return (
+      <div id='app' className='main-container'>
+        <Switch>
+          <Route path='/' exact component={HomePage} />
+          <Route path='/login' exact component={LoginPage} />
+          <Route path='/signup' exact component={SignupPage} />
+          {/* <Route path="/resetpassword" exact component={ResetPasswordPage} /> */}
+          <PrivateRoute
+            path='/time/home'
+            component={TimeHomePage}
+            trips={this.state.trips}
+            handleNewTrip={this.handleNewTrip}
+            handleStateUpdate={this.handleStateUpdate}
+            handleDelete={this.handleDelete}
+            handleFetchState={this.handleFetchState}
+          />
+          {/* <PrivateRoute path="/time/trip" exact component={TripPage} /> */}
+          <PrivateRoute
+            path='/time/trip/:tripId'
+            component={TripPage}
+            handleFetchYelp={this.handleFetchYelp}
+            handleAddedActivity={this.handleAddedActivity}
+          />
+          <PrivateRoute path='/time/profile/:userid' component={Profile} />
+          {/* <PrivateRoute path="/time/activitylist" exact component={ActivityList} /> */}
+          <PrivateRoute path='*' component={NotFound} />
+        </Switch>
+      </div>
+    );
   }
 }
 /*render={(props) => 
---
+++ client/containers/time/Profile.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import NavBar from '../../components/NavBar';
+import Footer from '../../components/Footer';
+
+const Profile = () => {
+  return <div>Test</div>;
+};
+
+export default Profile;
